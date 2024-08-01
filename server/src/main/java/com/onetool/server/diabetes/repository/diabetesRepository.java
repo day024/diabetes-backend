ae046a7b9e153d9551d11ea1d4b2fb4152d16dee
DaYeong <day0_ng@naver.com>
2024-08-01 17:48:27 +0900

    refactor: service수정

--- server/src/main/java/com/onetool/server/diabetes/repository/diabetesRepository.java
+++ server/src/main/java/com/onetool/server/diabetes/repository/diabetesRepository.java
@@ -1,27 +1,9 @@
 package com.onetool.server.diabetes.repository;
 
-import com.onetool.server.blueprint.Blueprint;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import com.onetool.server.diabetes.Diabetes;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface diabetesRepository extends JpaRepository<Blueprint, Long> {
-
-    @Query(value = "SELECT b FROM Blueprint b WHERE b.blueprintName LIKE %:keyword% OR b.creatorName LIKE %:keyword%")
-    Page<Blueprint> findAllNameAndCreatorContaining(@Param("keyword") String keyword, Pageable pageable);
-
-    @Query(value = "SELECT b FROM Blueprint b WHERE b.categoryId = " +
-            "(SELECT f.id FROM FirstCategory f WHERE f.name = :first)")
-    Page<Blueprint> findAllByFirstCategory(@Param("first") String category, Pageable pageable);
-
-    @Query(value = "SELECT b FROM Blueprint b WHERE b.secondCategory = :second " +
-            "AND b.categoryId = (SELECT f.id FROM FirstCategory f WHERE f.name = :first)")
-    Page<Blueprint> findAllBySecondCategory(@Param("first") String firstCategory, @Param("second") String secondCategory, Pageable pageable);
-
-    @Query(value = "SELECT count(*) FROM Blueprint b")
-    Long countAllBlueprint();
+public interface DiabetesRepository extends JpaRepository<Diabetes, Long> {
 }
