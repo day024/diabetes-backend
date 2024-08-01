e9625ea499cab61df9b8bc748658b550aeba48e0
DaYeong <day0_ng@naver.com>
2024-08-02 03:12:41 +0900

    fix: category sql 추가후 test확인

--- server/src/main/java/com/onetool/server/category/FirstCategoryRepository.java
+++ server/src/main/java/com/onetool/server/category/FirstCategoryRepository.java
@@ -3,6 +3,8 @@ package com.onetool.server.category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FirstCategoryRepository extends JpaRepository<FirstCategory, Long> {
 }
